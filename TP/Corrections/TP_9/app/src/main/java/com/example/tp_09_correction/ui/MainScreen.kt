a015fec3836811b88d8b5eff89f9eec4a4cb230c
Jordan <jordan.hiertz@al-enterprise.com>
2025-04-27 23:12:20 +0200

    Fix skeleton TP09

--- TP/Corrections/TP_9/app/src/main/java/com/example/tp_09_correction/ui/MainScreen.kt
+++ TP/Corrections/TP_9/app/src/main/java/com/example/tp_09_correction/ui/MainScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
---
+++ TP/TP - 9 - Ressources/app/src/main/java/com/example/tp_09_architecture/ui/GameViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.tp_09_architecture.ui
+
+class GameViewModel {
+
+}
